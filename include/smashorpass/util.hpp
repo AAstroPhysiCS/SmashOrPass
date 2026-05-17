5b5ba2c40373b84d2ba65944c286cf11bb8b356d
alexdesander <alexander.heim@student.uibk.ac.at>
2026-05-17 13:54:35 +0200

    Added util.hpp with Result

---
+++ include/smashorpass/util.hpp
@@ -0,0 +1,27 @@
+#pragma once
+
+#include <expected>
+#include <type_traits>
+#include <utility>
+
+namespace sop_util {
+
+// ---- RESULT ------------------------------------------------
+
+template <class T, class E> using Result = std::expected<T, E>;
+
+template <class E> constexpr auto Err(E &&error) {
+    return std::unexpected<std::decay_t<E>>(std::forward<E>(error));
+}
+
+template <class T> constexpr auto Ok(T &&value) {
+    return std::forward<T>(value);
+}
+
+struct OkVoid {
+    template <class E> constexpr operator Result<void, E>() const { return {}; }
+};
+
+constexpr OkVoid Ok() { return {}; }
+
+} // namespace sop_util
